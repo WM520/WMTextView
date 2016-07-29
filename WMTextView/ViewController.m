355a361e21487d323ac582d3a351d1df2fcb4e3e
WM <1071597946@qq.com>
2016-07-29 15:43:55 +0800

    描述文件

--- WMTextView/ViewController.m
+++ WMTextView/ViewController.m
@@ -22,7 +22,7 @@
     self.customTextView = [[WMTextView alloc] init];
     _customTextView.frame = CGRectMake(0, 64, self.view.frame.size.width, self.view.frame.size.height - 64);
     _customTextView.delegate = self;
-    _customTextView.placehoder = @"如果你喜欢这个空间欢迎";
+    _customTextView.placehoder = @"如果你喜欢这个控件,欢迎来下载 https://github.com/WM520/WMTextView 顺便star我谢谢,O(∩_∩)O~如果你喜欢这个控件,欢迎来下载 https://github.com/WM520/WMTextView 顺便star我谢谢,O(∩_∩)O~如果你喜欢这个控件,欢迎来下载 https://github.com/WM520/WMTextView 顺便star我谢谢,O(∩_∩)O~";
     _customTextView.placehoderColor = [UIColor redColor];
     [self.view addSubview:_customTextView];
 }
